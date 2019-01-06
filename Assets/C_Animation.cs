286bcd4b14d9194e42e4bc17a4f397ee5d3ed61e
soapod <joshuabenavente@gmail.com>
2019-01-06 15:27:37 -0700

    Initial

---
+++ Assets/C_Animation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+[RequireComponent(typeof(C_Input))]
+[RequireComponent(typeof(Animator))]
+
+public class C_Animation : MonoBehaviour
+{
+    public string leftStickVParam;
+    public string leftStickHParam;
+    public string leftStickMagParam;
+    public string lTargetingParam;
+    public float movementDampening = .05f;
+
+    Animator _animator;
+    C_Input _cI;
+
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _cI = GetComponent<C_Input>();
+    }
+
+    void Update()
+    {
+        ApplyInput();
+    }
+
+    void ApplyInput()
+    {
+        _animator.SetFloat(leftStickHParam, _cI.leftStickHorizontal);
+        _animator.SetFloat(leftStickVParam, _cI.leftStickVertical);
+        _animator.SetFloat(leftStickMagParam, _cI.leftStickMagnitude, movementDampening, Time.deltaTime);
+        _animator.SetBool(lTargetingParam, _cI.lTargeting);
+    }
+}
---
+++ Assets/C_Input.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class C_Input : MonoBehaviour
+{
+    public float leftStickHorizontal;
+    public float leftStickVertical;
+    public float leftStickMagnitude;
+    public float leftTrigger;
+    [Range(0, 1f)]
+    public float lTargetingThreshold = .2f;
+    public bool lTargeting;
+
+    Player _player;
+    Vector2 _stickInput;
+
+
+    void Awake()
+    {
+        // Get the Rewired Player object for this player and keep it for the duration of the character's lifetime
+        _player = ReInput.players.GetPlayer(0);
+    }
+
+    private void Update()
+    {
+        GetInput();
+        ProcessInput();
+    }
+
+    void GetInput()
+    {
+        leftStickHorizontal = _player.GetAxis("Move Horizontal");
+        leftStickVertical = _player.GetAxis("Move Vertical");
+        leftTrigger = _player.GetAxis("L Targeting");
+    }
+
+    void ProcessInput()
+    {
+        _stickInput = new Vector2(leftStickHorizontal, leftStickVertical);
+        leftStickMagnitude = _stickInput.magnitude;
+
+        if (leftTrigger >= lTargetingThreshold)
+            lTargeting = true;
+        else
+            lTargeting = false;
+    }
+}
